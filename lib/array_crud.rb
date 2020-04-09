67dcc01ef642a5c093444c63a1131e53edc45384
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 15:48:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array3 = Array.new(4)
+  array3.push("nil")
 end
 
 def add_element_to_start_of_array(array, element)
