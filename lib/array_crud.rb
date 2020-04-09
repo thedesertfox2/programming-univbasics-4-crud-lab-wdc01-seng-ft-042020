65c2af30ce278e5fd3d599865656092d5b068261
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 16:10:29 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,51 @@
 def create_an_empty_array
-  
+  array1 = []
 end
 
 def create_an_array
-  
+  array2 = Array.new(4)
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
+  the_beatles << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
+  the_beatles.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
+  the_beatles << "arrays!"
+  the_beatles.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
+  the_beatles.unshift("wow")
+  the_beatles.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  the_beatles = ["john lennon", "ringo starr", "am", "george harrison"]
+  the_beatles[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
+  the_beatles.unshift("wow")
+  the_beatles[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
+  the_beatles << "arrays!"
+  the_beatles[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
+  the_beatles[4] = "totally"
 end
