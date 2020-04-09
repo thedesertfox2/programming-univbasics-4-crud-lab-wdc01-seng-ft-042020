a8e95d119e11106de4c3a80dc2c9abbf0706e6b5
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 16:05:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,6 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
+  the_beatles << "arrays!"
   the_beatles.pop
 end
 
