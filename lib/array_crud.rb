2575df365239419f11cfc20a829fc4a5cb05f676
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 16:09:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -46,5 +46,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
 end
