dd92e6da26e29473fe66d8cb4018df008ff7dde4
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 15:42:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  puts the_beatles = ["ringo starr", "john lennon", "paul mccartney", "george harrison"]
+  the_beatles[4] 
 end
 
 def add_element_to_end_of_array(array, element)
