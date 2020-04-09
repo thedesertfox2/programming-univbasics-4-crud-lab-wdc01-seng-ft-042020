def7f4f5530c770cec5c6597d136bdf236c82e01
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 15:52:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   the_beatles = ["john lennon", "ringo starr", "paul mccartney", "george harrison"]
-  array3 << "nil"
+  the_beatles << "no one"
 end
 
 def add_element_to_start_of_array(array, element)
